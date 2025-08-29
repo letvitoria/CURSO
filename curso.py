647c9db6b3604f1179753d33b278426175ec4d78
nameuser <leticia.vitoria@estudante.ifro.edu.brgit config --global user.email leticia.vitoria@estudante.ifro.edu.br>
2025-08-29 13:14:27 -0400

    Divisão

--- curso.py
+++ curso.py
@@ -15,3 +15,20 @@ _newfloat = 1.0
 #armazenando um valor booleano
 bool_condition = True
 print(hellomessage, first_name, caracter, _newfloat, bool_condition)
+
+# Operações Aritméticas
+#adição
+print(10+5)
+#subtração
+print(100-4)
+#multiplicação
+print(20*5)
+#potência
+print(2**4)
+#Divisão (float)
+#Retornar o valor decimal real da divisão
+print(20/4)
+print(20/3)
+
+#Resto da divisão:
+print(10%3)
