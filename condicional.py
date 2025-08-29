d9ac94e6b949fa60eca642dcf301ef4bee5c2904
nameuser <leticia.vitoria@estudante.ifro.edu.brgit config --global user.email leticia.vitoria@estudante.ifro.edu.br>
2025-08-29 13:26:24 -0400

    função

---
+++ condicional.py
@@ -0,0 +1,11 @@
+#Estrutura de condicional
+x = 8
+y = 9
+
+if (2*x == y):
+    print("y is duble of x")
+elif (x**2 == y):
+    print("y is the square of x")
+else:
+    print("Y is NOT double of x")
+    
---
+++ funcao.py
@@ -0,0 +1,24 @@
+number = int(input("Digite um número: "))
+
+if number == 0:
+    print("O número é zero")
+elif number == 1:
+    print("O número é um")
+elif number == 2:
+    print("O número é dois")
+elif number == 3:
+    print("O número é três")
+elif number == 4:
+    print("O número é quatro")
+elif number == 5:
+    print("O número é cinco")
+elif number == 6:
+    print("O número é seis")
+elif number == 7:
+    print("O número é sete")
+elif number == 8:
+    print("O número é oito")
+elif number == 9:
+    print("O número é nove")
+else:
+    print("O número não está entre 0 e 9")
