b715d54c496b07578e8515cf6bd7bc8913204c29
nameuser <leticia.vitoria@estudante.ifro.edu.brgit config --global user.email leticia.vitoria@estudante.ifro.edu.br>
2025-08-29 12:57:38 -0400

    variáveis

---
+++ curso.py
@@ -0,0 +1,17 @@
+#declarando variáveis
+width = 10
+Height = 5
+# in [] width - out[] 10
+print(width)
+
+#armazenando uma string
+hellomessage = "Hello World"
+first_name = "João"
+
+#armazenando um caractere
+caracter = "a"
+#armazenando um float 
+_newfloat = 1.0
+#armazenando um valor booleano
+bool_condition = True
+print(hellomessage, first_name, caracter, _newfloat, bool_condition)
