d5acb24a4e6c7f7f867ac4a06e88011b64169db9
nameuser <leticia.vitoria@estudante.ifro.edu.brgit config --global user.email leticia.vitoria@estudante.ifro.edu.br>
2025-09-02 11:01:29 -0400

    Organizando POO

--- POO.py
+++ POO.py
@@ -7,15 +7,23 @@ class Library(object):
         self.call_no = call_no
         self.checked_out = False
 
-my_book = Library("", "", 0, "")
-my_book.title = "Dom Quixote"
-my_book.author = "Miguel de Cervantes"
-my_book.pub_year = 1605
-my_book.call_no = "123.456 CERV"
-my_book.checked_out = False
+    def title_and_author(self):
+        return "{} {}: {}".format(self.author[1], self.author[0], self.title)
 
+    def __str__(self):
+        return "{} {}({}): {}".format(self.author[1], self.author[0], self.pub_year, self.title)
+
+    def __repr__(self):
+        return "<book: {} ({})>".format(self.title, self.call_no)
+
+# Criando e usando objetos fora da classe
+my_book = Library("Dom Quixote", "Miguel de Cervantes", 1605, "123.456 CERV")
 print("Título: " + my_book.title)
 print("Autor: " + my_book.author)
 print("Ano de publicação: " + str(my_book.pub_year))
 print("Número de chamada: " + my_book.call_no)
 print("Está emprestado? " + str(my_book.checked_out))
+
+new_book = Library("Harry Potter and the Sorcerer's Stone", ("Rowling", "J.K."), 1998, "PZ7.R79835")
+print(new_book.title_and_author())
+print(new_book)
